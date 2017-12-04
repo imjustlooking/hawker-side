3cbb3d14e81d3d4bdcab52725d9b543a5da9aea7
Gabriel <gabrielulu@gmail.com>
2017-12-04 17:35:47 +0800

    changed db format to nest item price

--- src/App.js
+++ src/App.js
@@ -1,39 +1,43 @@
 import React, { Component } from 'react'
-// import logo from './logo.svg';
 import './App.css'
 import firebase from './firebase.js'
 
 class App extends Component {
-  constructor(props) {
-       super(props);
-       this.state = { inputs: ['2'] };
-   }
-  handleSubmit(e) {
+  constructor () {
+    super()
+    this.state = { inputs: ['1'] }
+  }
+  handleSubmit (e) {
     e.preventDefault()
     const restaurantRef = firebase.database().ref('restaurants')
     const formData = Array.from(e.target.elements)
            .filter(el => el.name)
            .reduce((a, b) => ({...a, [b.name]: b.value}),
            {})
-    // console.log(formData)
+    console.log(formData)
     // console.log('number of food fields', Object.keys(formData).length - 1)
 
     let restaurant = {
       items: {},
       name: formData.user
     }
-    for (var i in formData) {
-      if (i !== 'user') {
-        restaurant.items[i] = formData[i]
+    var values = Object.values(formData)
+    values.splice(0, 1)
+
+    for (var i = 0; i < values.length; i++) {
+      if (i % 2 === 0) {
+        console.log(values[i])
+        restaurant.items[values[i]] = values[i + 1]
       }
     }
+    console.log(restaurant)
     restaurantRef.push(restaurant)
   }
-  appendInput() {
-        var newInput = `${this.state.inputs.length+2}`;
-        this.setState({ inputs: this.state.inputs.concat([newInput]) });
+  appendInput () {
+    var newInput = `${this.state.inputs.length + 1}`
+    this.setState({ inputs: this.state.inputs.concat([newInput]) })
   }
-  render() {
+  render () {
     return (
       <div className='app'>
         <header>
@@ -47,16 +51,15 @@ class App extends Component {
               <input type='text' name='user' placeholder="What's your restaurant name?" required />
               <input type='text' name='it0' placeholder='Dish 1 name' required />
               <input type='number' name='pr0' min='0.01' step='0.01' placeholder='Price: e.g. 10.00' required />
-              <div id="dynamicInput">
-                         {this.state.inputs.map(
-                           counter =>
-                           <div>
-                           <input type='text' name={'it' + counter} placeholder={'Dish ' + counter + ' name'} required />
-                           <input type='number' name={'pr' + counter} min='0.01' step='0.01' placeholder='Price: e.g. 10.00' required /> </div>)}
-              </div>
+              {this.state.inputs.map(
+                 (counter, index) =>
+                   <div key={index}>
+                     <input type='text' name={'it' + counter} placeholder={'Dish ' + counter + ' name'} required />
+                     <input type='number' name={'pr' + counter} min='0.01' step='0.01' placeholder='Price: e.g. 10.00' required />
+                   </div>)}
               <button type='submit'>Submit</button>
             </form>
-            <button onClick={() => this.appendInput() }>
+            <button onClick={() => this.appendInput()}>
                    Add dish
                </button>
           </section>
@@ -68,7 +71,7 @@ class App extends Component {
           </section>
         </div>
       </div>
-    );
+    )
   }
 }
-export default App;
+export default App
