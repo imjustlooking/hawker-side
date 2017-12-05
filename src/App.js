e64517cf7383c7cdbfe5786657f973ae7f671bca
Gabriel <gabrielulu@gmail.com>
2017-12-05 09:34:35 +0800

    added router links

--- src/App.js
+++ src/App.js
@@ -1,11 +1,84 @@
 import React, { Component } from 'react'
 import './App.css'
 import firebase from './firebase.js'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+
+const BasicExample = () => (
+  <Router>
+    <div>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/topics">Topics</Link></li>
+        <li><Link to="/menu">Menu</Link></li>
+      </ul>
+
+      <hr/>
+
+      <Route exact path="/" component={Home}/>
+      <Route path="/about" component={About}/>
+      <Route path="/topics" component={Topics}/>
+      <Route path="/menu" component={App}/>
+    </div>
+  </Router>
+)
+
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+)
+
+const About = () => (
+  <div>
+    <h2>About</h2>
+  </div>
+)
+
+const Topics = ({ match }) => (
+  <div>
+    <h2>Topics</h2>
+    <ul>
+      <li>
+        <Link to={`${match.url}/rendering`}>
+          Rendering with React
+        </Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/components`}>
+          Components
+        </Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/props-v-state`}>
+          Props v. State
+        </Link>
+      </li>
+    </ul>
+
+    <Route path={`${match.url}/:topicId`} component={Topic}/>
+    <Route exact path={match.url} render={() => (
+      <h3>Please select a topic.</h3>
+    )}/>
+  </div>
+)
+
+const Topic = ({ match }) => (
+  <div>
+    <h3>{match.params.topicId}</h3>
+  </div>
+)
 
 class App extends Component {
   constructor () {
     super()
-    this.state = { inputs: [] }
+    this.state = {
+      inputs: []
+    }
   }
   handleSubmit (e) {
     e.preventDefault()
@@ -46,6 +119,21 @@ class App extends Component {
           </div>
         </header>
         <div className='container'>
+          {/* <Router>
+            <div>
+              <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/about">About</Link></li>
+                <li><Link to="/topics">Topics</Link></li>
+              </ul>
+
+              <hr/>
+
+              <Route exact path="/" component={Home}/>
+              <Route path="/about" component={About}/>
+              <Route path="/topics" component={Topics}/>
+            </div>
+          </Router> */}
           <section className='add-item'>
             <form refs='form' onSubmit={e => this.handleSubmit(e)}>
               <input type='text' name='user' placeholder="What's your restaurant name?" required />
@@ -54,7 +142,7 @@ class App extends Component {
               {this.state.inputs.map(
                  (counter, index) =>
                    <div key={index}>
-                     <input type='text' name={'it' + counter} placeholder={'Dish ' + (counter+1) + ' name'} required />
+                     <input type='text' name={'it' + counter} placeholder={'Dish ' + (counter + 1) + ' name'} required />
                      <input type='number' name={'pr' + counter} min='0.01' step='0.01' placeholder='Price: e.g. 10.00' required />
                    </div>)}
               <button type='submit'>Submit</button>
@@ -74,4 +162,4 @@ class App extends Component {
     )
   }
 }
-export default App
+export default BasicExample
