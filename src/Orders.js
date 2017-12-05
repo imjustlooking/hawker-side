865b66e984aaec8732b4b7f2f42cd9fe4f06c49c
Gabriel <gabrielulu@gmail.com>
2017-12-05 19:51:15 +0800

    sort orders by hawker

--- src/Orders.js
+++ src/Orders.js
@@ -1,37 +1,63 @@
 import React, { Component } from 'react'
 import firebase from './firebase.js'
 
-// var starCountRef = firebase.database().ref('orders/' + postId + '/items');
-var orderRef = firebase.database().ref('orders')
-var orderObj
-orderRef.on('value', function (snapshot) {
-  orderObj = snapshot.val()
-  console.log('orderObj', orderObj[0].items[0])
-})
+
 class Orders extends Component {
-// writeUserData(userId, name, email, imageUrl) {
+// writeUserData (userId, name, email, imageUrl) {
 //   firebase.database().ref('users/' + userId).set({
 //     username: name,
 //     email: email,
-//     profile_picture : imageUrl
+//     profile_picture: imageUrl
 //   })
 //   console.log('reached final step')
 // }
   constructor () {
     super()
     this.state = {
-      order: {}
+      order: []
     }
+  }
+
+  setItemsState () {
+    var hawkerRef = firebase.database().ref().child('orders').orderByChild('H_id').equalTo('H02')
+    // var hawkerRef = firebase.database().ref().child('restaurants').orderByChild('name').equalTo('Food Republic')
+  //  var hawkerRef = firebase.database().ref().child('restaurants').orderByChild('name').equalTo("Food Republic")
+    hawkerRef.on('value', snap => {
+      // console.log('snap val', snap.val())
+
+      var removeEmptyEl = snap.val().filter(el => el)
+      // console.log('display', removeEmptyEl)
+      var orderChange = []
+
+      for (var j in removeEmptyEl) {
+        console.log('j overview', j, snap.val()[j])
+        console.log('items', removeEmptyEl[j].items)
+        orderChange[j] = [] // each order will occupy a separate array
+        for (var k in removeEmptyEl[j].items) {
+          console.log(removeEmptyEl[j].items[k].name, removeEmptyEl[j].items[k].quantity)
+          orderChange[j].push(removeEmptyEl[j].items[k].name)
+          orderChange[j].push(removeEmptyEl[j].items[k].quantity)
+        }
+      }
+
+      this.setState({
+        order: orderChange
+      })
+      console.log('state.order', this.state.order)
+    })
   }
 
 render () {
   return (
     <div>
-      <h1> test </h1>
-      <p> </p>
-      {/* <button onClick={() => this.writeUserData(2, 'gabriel', 'gabrielulu@gmail.com','hotmail.com')}> Click this to write data </button> */}
+      <h1> Orders </h1>
+      {/* <p> {this.state.order[0]}x {this.state.order[2]} </p> */}
+      {/* <button onClick={() => this.writeUserData(2, 'nikita', 'sinfulnikita@gmail.com', 'hotmail.com')}> Click this to write data </button> */}
     </div>
   )
 }
-}
+  componentDidMount () {
+    this.setItemsState()
+  }
+  }
 export default Orders
